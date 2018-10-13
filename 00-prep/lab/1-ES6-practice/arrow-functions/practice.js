8fee8b91b379564828fb7b04a1bc9afc83aeabbd
MandanaTajoddin <40220065+MandanaTajoddin@users.noreply.github.com>
2018-10-13 11:46:36 -0400

    prework

---
+++ 00-prep/lab/1-ES6-practice/arrow-functions/practice.js
@@ -0,0 +1,15 @@
+// let number = [100,5,35,42,70]
+// number.sort((a,b) => {
+//    return (a<b)
+// });
+
+let number = [100,5,35,42,70]
+number.sort((a,b) => {
+   return (a>b)
+});
+
+let str = ["Mandana", "Alyssa","Jalal"]
+str.sort((a,b)=>{
+    return a>b
+})
+
